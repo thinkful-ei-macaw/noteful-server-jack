1aaceae932c6ae85aef58a64307c77e565dacc2c
Jack Lansing <jack.lansing@live.com>
2020-04-06 14:50:24 -0400

    added folders service

---
+++ src/folders/folders-service.js
@@ -0,0 +1,22 @@
+const FoldersService = {
+  getAllFolders(knex) {
+    return knex('folders').select('*');
+  },
+  getById(knex, id) {
+    return knex('folders').select('*').where({ id }).first();
+  },
+  insertFolder(knex, newFolder) {
+    return knex('folders')
+      .insert(newFolder)
+      .returning('*')
+      .then(rows => rows[0]);
+  },
+  deleteFolder(knex, id) {
+    return knex('folders').where({ id }).delete();
+  },
+  updateFolder(knex, id, newFolderFields) {
+    return knex('folders').where({ id }).update(newFolderFields);
+  }
+};
+
+module.exports = FoldersService;
