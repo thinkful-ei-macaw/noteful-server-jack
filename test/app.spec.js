9d558febd5d30d7e27f8180e79a89fb722302043
Jack Lansing <jack.lansing@live.com>
2020-04-06 14:15:40 -0400

    added postgrator-cli knex pg xss as dependencies

---
+++ test/app.spec.js
@@ -0,0 +1,9 @@
+const app = require('../src/app');
+
+describe('App', () => {
+  it('GET / respons with 200 containg "Hello, world!"', () => {
+    return supertest(app)
+      .get('/')
+      .expect(200, 'Hello, world!');
+  });
+});
