6871ee01c5fb19c1d581cb3c521448f403019571
Jack Lansing <jack.lansing@live.com>
2020-04-07 11:57:42 -0400

    setup config for deploy

--- test/folders-endpoints.spec.js
+++ test/folders-endpoints.spec.js
@@ -8,7 +8,7 @@ describe('Folders endpoints', () => {
   before('make a knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     });
     app.set('db', db);
   });
--- test/notes-endpoints.spec.js
+++ test/notes-endpoints.spec.js
@@ -9,7 +9,7 @@ describe('notes endpoints', () => {
   before('make a knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     });
     app.set('db', db);
   });
